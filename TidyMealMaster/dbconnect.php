60b220c289a0c115f455bf221fbc0040af318682
SitiChowdhury <anuraktachowdhury@gamil.com>
2024-01-30 22:22:14 +0600

    First commit

--- TidyMealMaster/dbconnect.php
+++ TidyMealMaster/dbconnect.php
@@ -1,11 +1,10 @@
 <?php
-	$server = "localhost";
-	$user = "root";
-	$pass = "";
-	$db ="tidymealmaster";
-	$conn = new mysqli($server, $user, $pass, $db);
-	if ($conn->connect_error)
-	  {
-	  	echo "Failed to connect with database: " . $conn->connect_error;
-	  }
+$server = "localhost";
+$user = "root";
+$pass = "";
+$db = "tidymealmaster";
+$conn = new mysqli($server, $user, $pass, $db);
+if ($conn->connect_error) {
+	echo "Failed to connect with database: " . $conn->connect_error;
+}
 ?>
